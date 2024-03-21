b6f3f408e471d8b901acc6fff127deee272f4dcb
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-21 08:38:40 +0530

    first commit

---
+++ components/FilterRegion.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilterRegion = () => {
+  return (
+    <div>FilterRegion</div>
+  )
+}
+
+export default FilterRegion
---
+++ components/SearchCountries.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchCountries = () => {
+  return (
+    <div>SearchCountries</div>
+  )
+}
+
+export default SearchCountries
