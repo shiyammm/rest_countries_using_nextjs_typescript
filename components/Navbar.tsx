d9b966eb35adbcb586f4baabf95ff08206b9f370
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-03-26 21:34:50 +0530

    Change Link component to anchor tag.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { ModeToggle } from '@/components/Theme';
-import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <header>
       <nav className="flex justify-between px-10 items-center h-[100px]">
         <div className="space-y-1">
-          <Link href={`/`} className="text-xl font-semibold block">
+          <a href="/" className="text-xl font-semibold block">
             CountryInsight
-          </Link>
+          </a>
           <span className="text-gray-500 text-sm block">
             Viewing the World's Colors{' '}
           </span>
